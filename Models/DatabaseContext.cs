f9b10f0e6301b6ba7e448517e6e99fd20073f8bb
Kristy <kprieto14@gmail.com>
2024-03-03 19:12:17 -0500

    Add database and related class.

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -8,13 +8,13 @@ namespace TacoTuesday.Models
     public partial class DatabaseContext : DbContext
     {
         // Change this if you want to have a different database name in development
-        private static string DEVELOPMENT_DATABASE_NAME = "TacoTuesdayDatabase";
+        private static string DEVELOPMENT_DATABASE_NAME = "MyAppDatabase";
 
         // Change this to true if you want to have logging of SQL statements in development
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
+        public DbSet<Restaurant> Restaurants { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
